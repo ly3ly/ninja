e52482b10d72c0392a8b5656e4b387b88a539d2b
Yzzrz924 <zhurunze86@gmail.com>
2023-11-13 18:32:11 +0800

    auth patch

--- ninja-web/src/services/tools.js
+++ ninja-web/src/services/tools.js
@@ -1,30 +1,29 @@
-import logo from '../assets/logo.svg';
+import logo from "../assets/logo.svg";
 
 export const defaultImg = logo;
 
 /**
  * 服务器地址
  */
-export const serverUrl = 'http://localhost:3000';
+export const serverUrl = "http://localhost:3000";
 
 /**
  * 文件上传接口
  */
-export const uploadActionUrl = serverUrl + '/common/upload';
+export const uploadActionUrl = serverUrl + "/common/upload";
 
 /**
  * 设置token
  * @param token
  * @returns
  */
-export const setToken = (token) =>
-    sessionStorage.setItem('token', token);
+export const setToken = (token) => sessionStorage.setItem("token", token);
 
 /**
  * 获取token
  * @returns
  */
-export const getToken = () => sessionStorage.getItem('token');
+export const getToken = () => sessionStorage.getItem("token");
 
 /**
  * 图片处理
@@ -32,9 +31,9 @@ export const getToken = () => sessionStorage.getItem('token');
  * @returns
  */
 export const dalImg = (img) => {
-    if (img) {
-        if (img.startsWith('http')) return img;
-        return serverUrl + img;
-    }
-    return defaultImg;
+  if (img) {
+    if (img.startsWith("http")) return img;
+    return serverUrl + img;
+  }
+  return defaultImg;
 };
